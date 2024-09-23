a4ba3593f75b81c82f9c263983c62eadecc3a9d8
Tunay Özdemir <tunay.ozdemir@consultant.turkcell.com.tr>
2024-09-23 06:45:11 +0300

    bugfix

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,4 +1,4 @@
-export { default as Weater } from './weather/page'
+export { default as Weater } from './weather'
 export { default as SelectBox } from './selectionBox'
 export { default as Background } from './bg'
 export { default as Loading } from './loading'
